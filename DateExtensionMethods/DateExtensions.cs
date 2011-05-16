97dfeff7e3c7c941b883d529e6bc78323e472392
unknown <mike.roth@.(none)>
2011-05-15 20:36:36 -0700

    Extensions for dateTime so that you can get stuff done

---
+++ DateExtensionMethods/DateExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateExtensionMethods
+{
+    public static  class StringDateExtensions
+    {
+        
+        public static DateTime ToDate(this string dateFormated)
+        {
+            return DateTime.Parse(dateFormated);
+        }
+    }
+
+    public static class DateExtensions
+    {
+        public static int LastDayInMonth(this DateTime date)
+        {
+            string rootMonthString = string.Format("{0}/1/{1}", date.Month, date.Year);
+            return rootMonthString.ToDate().AddMonths(1).AddDays(-1).Day;
+        }
+        
+        public static bool IsLastDayInMonth(this DateTime date)
+        {
+            return date.Day == date.LastDayInMonth();
+
+        }
+
+        public static DateTime NextMonth(this DateTime startDate)
+        {
+            return GetResultingDate(startDate, 1);
+        }
+
+        public static DateTime LastMonth(this DateTime startDate)
+        {
+            return GetResultingDate(startDate, -1);
+        }
+
+        private static DateTime GetResultingDate(DateTime startDate, int monthLocation)
+        {
+            DateTime nextMonth = startDate.AddMonths(monthLocation);
+            DateTime resultingDate;
+
+            if (startDate.Day > nextMonth.LastDayInMonth())
+                resultingDate = string.Format("{0}/{1}/{2}", nextMonth.Month, nextMonth.LastDayInMonth(), nextMonth.Year).ToDate();
+            else
+                resultingDate = string.Format("{0}/{1}/{2}", nextMonth.Month, startDate.Day, nextMonth.Year).ToDate();
+            return resultingDate;
+        }
+    }
+}
---
+++ Tests/DateExtensionMethodsTest.cs
@@ -0,0 +1,81 @@
+﻿
+
+
+
+namespace Tests
+{
+
+    using System;
+    using NUnit.Framework;
+    using DateExtensionMethods;
+
+    [TestFixture]
+    public class StringDateExtensionTests
+    {
+        [Test()]
+        public void If_given_a_valid_date_should_resolve_to_a_date()
+        {
+            var shouldBe = DateTime.Parse("10/3/2011");
+            var actual = "10/3/2011".ToDate();
+
+            Assert.AreEqual(shouldBe, actual);
+        }
+
+        [Test]
+        [ExpectedException(typeof(FormatException))]
+        public void If_given_a_invalid_date_should_throw_exception()
+        {
+            var actual = "10/32/2011".ToDate();
+        }
+
+    }
+
+    [TestFixture]
+    public class DateExtensions
+    {
+        [Test]
+        public void If_given_a_date_should_return_as_an_int_the_last_day_of_that_month()
+        {
+            var shouldBe = 31;
+            var actual = "1/1/2011".ToDate().LastDayInMonth();
+
+            Assert.AreEqual(shouldBe, actual);
+        }
+
+        [Test]
+        public void If_given_a_date_that_is_valid_should_be_able_to_determine_if_its_the_last_day_of_month()
+        {
+            var actual = "1/31/2011".ToDate().IsLastDayInMonth();
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void If_given_a_date_that_is_valid_should_be_able_to_determine_its_next_aniversary_within_a_month()
+        {
+            var actual = "1/31/2011".ToDate().NextMonth();
+            var shouldBe = "2/28/2011".ToDate();
+
+            Assert.AreEqual(shouldBe, actual);
+        }
+        
+        [Test]
+        public void If_given_a_date_that_is_valid_next_month_should_return_the_next_months_date()
+        {
+            var actual = "1/1/2011".ToDate().NextMonth();
+            var shouldBe = "2/1/2011".ToDate();
+
+            Assert.AreEqual(shouldBe, actual);
+        }
+
+        [Test]
+        public void If_given_a_date_that_is_valid_last_month_should_return_the_last_month_date()
+        {
+            var actual = "1/1/2011".ToDate().LastMonth();
+            var shouldBe = "12/1/2010".ToDate();
+
+            Assert.AreEqual(shouldBe, actual);
+        }
+    }
+
+}
